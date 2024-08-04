5ae1ed169b9a98935952dede4efe375cde4bae70
Ruben <rubenramirez121212@gmail.com>
2024-08-04 14:39:23 -0700

    landing page initial

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -1,11 +1,22 @@
 import "./LandingPage.css";
+import { useNavigate } from "react-router-dom";
 
 export default function LandingPage() {
+  const navigate = useNavigate()
+
+  const handleLogin = () => {
+    navigate("/login")
+  }
+
+  const handleSignup = () => {
+    navigate("/signup")
+  }
+
   return (
     <div className="landing-page__div">
       <div className="landing-page__navbar">
-        <button>Login</button>
-        <button>Signup</button>
+        <button className="landing-page__nav-button" onClick={handleLogin}>Log in</button>
+        <button className="landing-page__nav-button" onClick={handleSignup}>Sign up</button>
       </div>
       <div className="landing-page__main-content">
         <div className="landing-page__main-content-info">
@@ -16,8 +27,8 @@ export default function LandingPage() {
           </p>
         </div>
         <div className="landing-page__buttons">
-          <button>Signup</button>
-          <button>Login</button>
+          <button className="landing-page__main-button" onClick={handleSignup}>Sign up now</button>
+          <button className="landing-page__main-button" onClick={handleLogin}>Log in</button>
         </div>
       </div>
     </div>
