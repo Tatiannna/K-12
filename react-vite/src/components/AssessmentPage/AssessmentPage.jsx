6bfcb3dd3188bc05011b5040631bc4a7e8c637fa
hector <crespomhector@gmail.com>
2024-08-06 22:18:46 -0400

    completed the essential functionalities of the assessments user story. Users can select a grade and a subject, which will generate an assessment from the Gemini AI model. Users can submit the assessment to receive a grade.

--- react-vite/src/components/AssessmentPage/AssessmentPage.jsx
+++ react-vite/src/components/AssessmentPage/AssessmentPage.jsx
@@ -7,11 +7,6 @@ import "./AssessmentPage.css";
 import AssessmentImage from "../../Images/k12AssessmentImage.png";
 
 function AssessmentPage() {
-
-    // redirect user back to the / page if not logged in.
-    const sessionUser = useSelector((state) => state.session.user);
-    if (!sessionUser) return <Navigate to="/" replace={true} />;
-
     const { gradeLevel, subject } = useParams();
     // handle the loading percentage
     const [load, setLoad] = useState(0);
@@ -19,6 +14,10 @@ function AssessmentPage() {
     const [isLoading, setIsLoading] = useState(true);
     // handle begin test button
     const [startTest, setStartTest] = useState(false);
+    // handle total correct answer
+    const [correct, setCorrect] = useState(0);
+    // handle disable button
+    const [disable, setDisable] = useState(false);
     // handle data from fetch request
     const [data, setData] = useState(null);
 
@@ -46,23 +45,22 @@ function AssessmentPage() {
                     throw new Error("Network response went wrong, please try again.")
                 }
 
-                // setTimeout(() => {
-                //     clearInterval(intervalID);
-                //     setLoad((prev) => prev = 100);
-                //     setIsLoading(false);
-                // }, 4000)
+                setTimeout(() => {
+                    clearInterval(intervalID);
+                    setLoad((prev) => prev = 100);
+                    setIsLoading(false);
+                }, 4000)
 
                 const data = await res.json();
                 setData(data);
-                console.log('data: ', data);
             }
             catch (err) {
                 console.log(err);
             }
             finally {
-                clearInterval(intervalID);
-                setLoad((prev) => prev = 100);
-                setIsLoading(false);
+                // clearInterval(intervalID);
+                // setLoad((prev) => prev = 100);
+                // setIsLoading(false);
             }
         }
 
@@ -73,9 +71,28 @@ function AssessmentPage() {
         return () => clearInterval(intervalID);
     }, [isLoading])
 
+    // redirect user back to the / page if not logged in.
+    const sessionUser = useSelector((state) => state.session.user);
+    if (!sessionUser) return <Navigate to="/" replace={true} />;
+
     const startAssessment = (e) => {
         // when clicked, hide the progress bar
         setStartTest(true);
+    }
+
+    const submitTest = (e) => {
+        e.preventDefault();
+        // disable submit button
+        setDisable(true);
+        // grade each multiple choice
+        const formData = new FormData(e.target);
+        data?.questions.forEach((question, idx) => {
+            const selectedAnswer = formData.get(`question${idx}`);
+            // compare selected answer
+            if (selectedAnswer === question.answer) {
+                setCorrect(prev => prev += 1);
+            }
+        })
     }
 
     return (
@@ -83,31 +100,51 @@ function AssessmentPage() {
             <NavBar />
             <div className={startTest ? "wrapper" : "hide"}>
                 {/* Side Note. Store the response in a use state to grade the assessment*/}
-                <form className="form">
+                <form onSubmit={submitTest} className="form">
                     {/* map through the list of questions from the res */}
                     {data?.questions.map((question, idx) => (
                         <fieldset key={idx}>
-                            <legend>{`Question #${idx+1}: ${question.question}`}</legend>
+                            <legend>{`Question #${idx + 1}: ${question.question}`}</legend>
                             <div>
-                                <input type="radio" name={`question${idx}`} id={`q${idx}-a1`} />
+                                <input type="radio" name={`question${idx}`} id={`q${idx}-a1`} value={question.options[0]} required />
                                 <label htmlFor={`q${idx}-a1`}>{question.options[0]}</label>
+                                <div className={disable ? "mark-wrapper" : "hide"} >
+                                    <span className={question.options[0] === question.answer ? "correct" : "incorrect"}></span>
+                                </div>
                             </div>
                             <div>
-                                <input type="radio" name={`question${idx}`} id={`q${idx}-a2`} />
+                                <input type="radio" name={`question${idx}`} id={`q${idx}-a2`} value={question.options[1]} required />
                                 <label htmlFor={`q${idx}-a2`}>{question.options[1]}</label>
+                                <div className={disable ? "mark-wrapper" : "hide"} >
+                                    <span className={question.options[1] === question.answer ? "correct" : "incorrect"}></span>
+                                </div>
                             </div>
                             <div>
-                                <input type="radio" name={`question${idx}`} id={`q${idx}-a3`} />
+                                <input type="radio" name={`question${idx}`} id={`q${idx}-a3`} value={question.options[2]} required />
                                 <label htmlFor={`q${idx}-a3`}>{question.options[2]}</label>
+                                <div className={disable ? "mark-wrapper" : "hide"} >
+                                    <span className={question.options[2] === question.answer ? "correct" : "incorrect"}></span>
+                                </div>
                             </div>
                             <div>
-                                <input type="radio" name={`question${idx}`} id={`q${idx}-a4`} />
+                                <input type="radio" name={`question${idx}`} id={`q${idx}-a4`} value={question.options[3]} required />
                                 <label htmlFor={`q${idx}-a4`}>{question.options[3]}</label>
+                                <div className={disable ? "mark-wrapper" : "hide"} >
+                                    <span className={question.options[3] === question.answer ? "correct" : "incorrect"}></span>
+                                </div>
                             </div>
                         </fieldset>
                     ))}
-                    <button type="submit" className="submit">Submit</button>
+                    <button type="submit" disabled={disable} className="submit">Submit</button>
                 </form>
+                <h2 className={disable ? "score" : "hide"}>
+                    Score: {parseInt((correct / data?.questions.length) * 100)}
+                </h2>
+                {correct === data?.questions.length && (
+                    <div>
+                        🚀 Hold the Phone! 🚀 A perfect 100%? Are you secretly a robot sent from the future to ace every test? 🤖💯 Well done! 🌟
+                    </div>
+                )}
             </div>
             <div className={startTest ? "hide" : "wrapper"}>
                 <img className="test-image" src={AssessmentImage} alt="Test Exam Image" />
