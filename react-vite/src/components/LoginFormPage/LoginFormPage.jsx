38a94469954364bf0eab7a19c4740ff877d16a90
Tatianna <tatirobinson@gmail.com>
2024-08-25 16:23:32 -0700

    Fix indentation, add styling

--- react-vite/src/components/LoginFormPage/LoginFormPage.jsx
+++ react-vite/src/components/LoginFormPage/LoginFormPage.jsx
@@ -48,55 +48,56 @@ function LoginFormPage() {
   return (
     <>
       <div className="landing-page__div">
-      <div className="landing-page__navbar"></div>
-      <div className="landing-page__main-content">
-        <div className="landing-page__main-content-info">
-          <h1>Log In</h1>
-        </div>
-      {errors.length > 0 &&
-        errors.map((message) => <p className="error" key={message}>{message}</p>)}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label className="form-label">
-            Email
-            <div>
+        <div className="landing-page__navbar"></div>
+        <div className="landing-page__main-content">
+          <div className="landing-page__main-content-info">
+            <h1>Log In</h1>
+          </div>
+          {errors.length > 0 &&
+          errors.map((message) => <p className="error" key={message}>{message}</p>)}
+          
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label className="form-label">
+              Email
+              <div>
+                <input
+                  className="form-input"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+            </label>
+          </div>
+          
+          {errors.email && <p className="error">{errors.email}</p>}
+          <div>
+
+            <label className="form-label">
+              Password
+              <div>
               <input
                 className="form-input"
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 required
               />
+              </div>
+              
+            </label>
             </div>
-          </label>
-        </div>
-        
-        {errors.email && <p className="error">{errors.email}</p>}
-        <div>
-
-          <label className="form-label">
-            Password
-            <div>
-            <input
-              className="form-input"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            </div>
-            
-          </label>
+          {errors.password && <p className="error">{errors.password}</p>}
+          <div className='login-button-container'>
+            <button className="landing-page__main-button" type="submit">Log In</button>
           </div>
-        {errors.password && <p className="error">{errors.password}</p>}
-        <div className='login-button-container'>
-          <button className="landing-page__main-button" type="submit">Log In</button>
+          <div className='login-button-container'>
+            <button className="landing-page__main-button" onClick={demoLogin}>Demo User</button>
+          </div>
+        </form>
         </div>
-        <div className='login-button-container'>
-          <button className="landing-page__main-button" onClick={demoLogin}>Demo User</button>
-        </div>
-      </form>
-      </div>
       </div>
     </>
   );
