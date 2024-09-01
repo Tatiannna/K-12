d798e9f19940a0a02d75735c2fd1b34ff17985ff
Tatianna <tatirobinson@gmail.com>
2024-08-31 21:22:39 -0700

    Add id, grade, subject to assessments schema

--- app/models/assessments.py
+++ app/models/assessments.py
@@ -1,3 +1,19 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+
+class Assessment(db.Model):
+
+    __tablename__ = 'assessments'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(255), nullable=False)
+    grade = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'subject': self.subject,
+            'grade': self.grade
+        }
