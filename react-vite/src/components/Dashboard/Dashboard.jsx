67b27722b6a223973edfcb1f3942aefbfd20c37a
Tatianna <tatirobinson@gmail.com>
2024-09-03 21:08:58 -0700

    Fetch and render currentUser Assessment history

--- react-vite/src/components/Dashboard/Dashboard.jsx
+++ react-vite/src/components/Dashboard/Dashboard.jsx
@@ -1,16 +1,26 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAssessments } from "../../redux/assessments";
 import './Dashboard.css'
-
 
 
 const Dashboard = () => {
 
     const dispatch = useDispatch();
-    
+    const assessments = useSelector(state => state.assessments);
+    const currentUser = useSelector(state => state.session.user);
+
+    useEffect( () => {
+        dispatch(getAssessments(currentUser.id));
+    }, [dispatch, currentUser] );
+
     return(
         <>
-            <h1>Dashboard!</h1>
+            <h1>My Grade History</h1>
+            <ul>
+                {Object.values(assessments).map(assessment => (
+                <li>{assessment.subject} {assessment.grade} </li>))}
+            </ul>
         </>
     );
 }
