de2455c8cb7edcab2031c6d1983572927d8986f6
Tatianna <tatirobinson@gmail.com>
2024-09-04 11:18:27 -0700

    Calculate assessments metrics, show on dashboard

--- react-vite/src/components/Dashboard/Dashboard.jsx
+++ react-vite/src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect} from "react";
+import { useEffect, useState} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAssessments } from "../../redux/assessments";
 import './Dashboard.css'
@@ -9,17 +9,63 @@ const Dashboard = () => {
     const dispatch = useDispatch();
     const assessments = useSelector(state => state.assessments);
     const currentUser = useSelector(state => state.session.user);
+    const [stats, setStats] = useState(null);
 
     useEffect( () => {
         dispatch(getAssessments(currentUser.id));
+        
     }, [dispatch, currentUser] );
+
+    useEffect( () => {
+        setStats(calculateStats())
+    }, [assessments])
+
+    const assessmentsArray = Object.values(assessments);
+
+    const calculateStats = () => {
+        let numAssessmentsTaken = assessmentsArray.length;
+        let numMathTaken = 0;
+        let numReadingTaken = 0;
+        let averageMathScore = 0;
+        let averageReadingScore = 0;
+        let totalMathScore = 0;
+        let totalReadingScore = 0;
+
+        console.log("assessments array: ", assessmentsArray);
+        for (let i = 0; i < assessmentsArray.length; i++){
+            if (assessmentsArray[i].subject == 'Mathematics'){
+                numMathTaken++;
+                totalMathScore += assessmentsArray[i].grade;
+            }else if (assessmentsArray[i].subject == 'Reading'){
+                numReadingTaken++;
+                totalReadingScore += assessmentsArray[i].grade;
+            }
+        }
+
+        averageMathScore = totalMathScore / numMathTaken;
+        averageReadingScore = totalReadingScore / numReadingTaken;
+
+        return {
+            numAssessmentsTaken,
+            numMathTaken,
+            numReadingTaken,
+            averageMathScore,
+            averageReadingScore,
+        }
+    }
 
     return(
         <>
-            <h1>My Grade History</h1>
+            <h1>My Assessment Summary</h1>
+            <p>Assessments Taken: {stats.numAssessmentsTaken}</p>
+            <p>Math Assessments Taken: {stats.numMathTaken}</p>
+            <p>Reading Assessments Taken: {stats.numReadingTaken}</p>
+            <p>Average Math Score: {stats.averageMathScore}</p>
+            <p>Average Reading Score: {stats.averageReadingScore}</p>
+            <p>Grades: </p>
             <ul>
-                {Object.values(assessments).map(assessment => (
-                <li>{assessment.subject} {assessment.grade} </li>))}
+                {assessmentsArray.map(assessment => (
+                <li key={assessment.id}>{assessment.subject} {assessment.grade} </li>))}
             </ul>
         </>
     );
