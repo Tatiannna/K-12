4cc093d711cf5935d000de467154d911a5b3ab89
Tatianna <tatirobinson@gmail.com>
2024-09-04 17:45:46 -0700

    Update Assessments schema to include grade lvl and date taken

--- app/models/assessment.py
+++ app/models/assessment.py
@@ -1,5 +1,6 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from .user import User
+from datetime import datetime
 
 class Assessment(db.Model):
 
@@ -8,13 +9,18 @@ class Assessment(db.Model):
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
+    now = datetime.utcnow()
+    formatted_date = now.strftime('%Y-%m-%d %H:%M:%S.%f')
+
+
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(255), nullable=False)
-    grade = db.Column(db.Integer, nullable=False)
+    grade_level = db.Column(db.Integer, nullable=False)
+    score = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    date_taken = db.Column(db.String(255), default=formatted_date, nullable=False)
 
     user = db.relationship('User', backref=db.backref('assessments', lazy=True))
-
 
     def to_dict(self):
         return {
